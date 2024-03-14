4151cb990e4f8acdbb343f86a6bf229c0523fa6c
Jack Bennett <jabennetto@yahoo.co.uk>
2024-03-14 13:15:46 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='container'>
+        <h2>Frequently Asked Questions</h2>
+        {AccordionUsage()}
+      </div>
+    </div>
+  );
+}
+
+function AccordionUsage() {
+  // Array to hold the data for each accordion, ideally this would be passed as a parameter to this function
+  const accordions = [
+    { id: 1, heading: 'Accordion 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { id: 2, heading: 'Accordion 2', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { id: 3, heading: 'Accordion 3', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { id: 4, heading: 'Accordion 4', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' },
+    { id: 5, heading: 'Accordion 5', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.' }
+  ];
+
+  return (
+    <div>
+      {accordions.map(accordion => (
+        <Accordion key={accordion.id}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel${accordion.id}-content`}
+            id={`panel${accordion.id}-header`}
+          >
+            {accordion.heading}
+          </AccordionSummary>
+          <AccordionDetails>
+            {accordion.content}
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </div>
   );
 }
